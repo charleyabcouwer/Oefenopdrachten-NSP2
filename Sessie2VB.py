18c2215de1ae1bcefb565f0a0307c5dbea13f901
charleyabcouwer <charlotte.abcouwer@gmail.com>
2025-10-30 22:21:02 +0100

    Final version voorbereiding Sessie 2
    Bekijk de conversie codes voor ACD --> spanning en omgekeerd én de overige opgaven die voor de voorbereiding gemaakt moesten worden.

--- Sessie2VB.py
+++ Sessie2VB.py
@@ -24,3 +24,13 @@ print(f"Spanning bij ADC={adc_test}: {spanning:} V.")
 print(f"ADC waarde bij {spanning_test} V: {adc}.")
 
 # Uitkomsten zijn ongeveer zoals verwacht maar nog dubbelchecken !!
+
+# Ik denk dat een I-U diagram van een LED een soort exponentiële vorm zal hebben en zal beginnen bij 0. 
+# Zodra de drempelspanning is overschreven, zal de stroom snel toenemen met de U. 
+
+# Voor de U = U_LED weten we dat U = I*R. Daarnaast, U_LED = U_1 - U_2 er vanuitgaande dat U_1 de 
+# Hoofdspanningbron is. Aangezien de enige spanning die bepaald hoe groot de stroom is is de spanning
+# Over de weerstand: I = U_2 / R. 
+
+# Ik heb de opdrachten gemaakt tot Arduino op breadboard omdat ik vanaf daar niet verder kon
+# (niet tot Kanalen van Arduino). 
