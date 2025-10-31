df27a2a51b35356cb27a156b19ceb7cf2113e268
charleyabcouwer <charlotte.abcouwer@gmail.com>
2025-10-31 22:05:47 +0100

    CSV bestand schrijven test

--- Sessie2_csv.py
+++ Sessie2_csv.py
@@ -1,13 +1,19 @@
-Lijst_kracht = [1.2, 1.8, 2.4, 2.7, 3.1] # N
-Lijst_afstand = [0.3, 0.4, 0.6, 0.8, 1.0] # m 
+import csv
 
-# repeat
-#   print F, s, W
 
-for F, s in zip(Lijst_kracht, Lijst_afstand):
+# In deze subopgave maak ik van python code een csv bestand.
 
-    kracht = F
-    afstand = s
-    arbeid = F * s
+Lijst_kracht = [1.2, 1.8, 2.4, 2.7, 3.1]  
+Lijst_afstand = [0.3, 0.4, 0.6, 0.8, 1.0]  
 
-    print(f"Kracht: {kracht} N, Afstand: {afstand} m, Arbeid:{arbeid} W")
+# CSV-bestand aanmaken en vullen
+with open('arbeid_metingen.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Kracht (N)", "Afstand (m)", "Arbeid (J)"])  # kolomkoppen
+
+    for F, s in zip(Lijst_kracht, Lijst_afstand):
+        arbeid = F * s
+        print(f"Kracht: {F} N, Afstand: {s} m, Arbeid: {arbeid:} J")
+        writer.writerow([F, s, arbeid])
+
+print("CSV-bestand 'arbeid_metingen.csv' aangemaakt, zie bestanden.")
