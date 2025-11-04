4557d81bd29a84ba203250815bf4501fdd2ec50b
charleyabcouwer <charlotte.abcouwer@gmail.com>
2025-11-04 11:22:37 +0100

    Nieuwe opdrachten

--- Sessie3_temperature.py
+++ Sessie3_temperature.py
@@ -5,31 +5,34 @@ class Temperature:
 
     def to_celsius(self):
 
-        C = temperature
-        
+        C = self.temperature
         # print(f"De temperatuur in celsius is {self.temperature} graden Celsius.")
+        return C
 
     def to_kelvin(self):
-        K = temperature + 273.15
+        K = self.temperature + 273.15
         # print(f"De temperatuur in kelvin is {K} K.")
+        return K
 
     def to_fahrenheit(self):
-        F = temperature * 9/5 + 32
+        F = (self.temperature * 9/5) + 32
         # print(f"De temperatuur in Fahrenheit is {F} graden Fahrenheit.")
+        return F
 
     def update_temperature(self, temperature_celsius):
         # update the stored temperature
-        ...
+        self.temperature = temperature_celsius
+        
 
 temperature = Temperature(37) # Instance
 
 print(f"{temperature.temperature=}")
 print(f"{temperature.to_celsius()=}")
 print(f"{temperature.to_kelvin()=}")
-print(f"{temperature.to_fahrenheit()=}")
-print()
+print(f"{temperature.to_fahrenheit()=} \n")
 
 temperature.update_temperature(21)
+
 print(f"{temperature.to_celsius()=}")
 print(f"{temperature.to_kelvin()=}")
 print(f"{temperature.to_fahrenheit()=}")
