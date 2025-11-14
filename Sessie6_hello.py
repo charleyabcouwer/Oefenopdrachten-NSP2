de2b801ab7a86709d7899579c50135545339445a
charleyabcouwer <charlotte.abcouwer@gmail.com>
2025-11-14 14:04:12 +0100

    Vlag toevoegen

--- Sessie6_hello.py
+++ Sessie6_hello.py
@@ -12,11 +12,26 @@ import click
     help="Number of times to print greeting.",
     show_default=True,  # show default in help
 )
-def hello(name, count):
+@click.option("--tea/--no-tea", default=True)
+def hello(name, count, tea):
     for _ in range(count):
         print(f"Hello {name}!")
+        if tea:
+            print("Wil je thee?")
         time.sleep(2)  # Wait 2 seconds
 
 
 if __name__ == "__main__":
     hello()
+
+# make boolean flag tea/no-tea
+
+
+# function, parameter name, count, pause and tea
+# repeat count times
+# print hello <name>!
+# if wished offer tea
+# pause
+
+# when run this script:
+# run function
